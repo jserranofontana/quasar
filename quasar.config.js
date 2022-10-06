c006c6eaf3a5d4f47ed900d83ca34b60ab2a5963
Juan Manuel Serrano Fontana <jserranofontana@yahoo.es>
2022-10-06 22:18:16 +0200

    update pwa config

--- quasar.config.js
+++ quasar.config.js
@@ -172,11 +172,11 @@ module.exports = configure(function (ctx) {
       manifest: {
         name: 'Quasar Estoque Supabase',
         short_name: 'Quasar Estoque Supabase',
-        description: 'A Quasar Framework app',
+        description: 'Estoque fácil na palma da sua mao',
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#2c9c6a',
         icons: [{
           src: 'icons/icon-128x128.png',
           sizes: '128x128',
